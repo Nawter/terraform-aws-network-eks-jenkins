e38f164191e47d7c47c83b5e946e0fe47ed812ce
nawter <772025+Nawter@users.noreply.github.com>
2022-11-13 14:15:40 +0000

    clean and prepare the repo

--- ambassador.tf
+++
@@ -1,6 +0,0 @@
-#resource "helm_release" "ambassador" {
-#  name  = "emissary-ingress"
-#  chart = "https://app.getambassador.io/"
-#  namespace        = "ambassador"
-#  create_namespace = true
-#}
